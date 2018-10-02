acd0efd484fba697e6949d8de5fc9c1fd58fad84
fmaliha <43738990+fmaliha@users.noreply.github.com>
2018-10-02 21:06:58 +0600

    Add files via upload

---
+++ Lab3 CSS/Index.php
@@ -0,0 +1,19 @@
+<?PHP
+echo"Hello<br>";
+
+
+ $var1 =3;
+
+echo$var1;
+for($i=0;$i<9;$i++)
+{
+	if($i/$2 ==0)
+	{echo "<br>".$i.-even" <br>";
+	}
+}
+else
+{
+	echo"odd"
+}
+
+?>
---
+++ Lab3 CSS/login.php
@@ -0,0 +1,12 @@
+
+<?PHP
+
+
+
+$user=$_POST['un'];
+$pass=$_POST['pw'];
+echo"user name:".$user;
+echo"Password:".$pass;
+
+
+?>
